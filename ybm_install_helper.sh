1a0207bbda4d719dd2c7ac540cac720874700543
rdcyosuzume <fost008@gmail.com>
2022-05-28 16:51:14 +0000

    add install bash

---
+++ ybm_install_helper.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/rdcyosuzume/mysqldump_shell/main/FTP_send.sh
+wget https://raw.githubusercontent.com/rdcyosuzume/mysqldump_shell/main/README.md
+wget https://raw.githubusercontent.com/rdcyosuzume/mysqldump_shell/main/backup_order.sh
+wget https://raw.githubusercontent.com/rdcyosuzume/mysqldump_shell/main/ybm
+wget https://raw.githubusercontent.com/rdcyosuzume/mysqldump_shell/main/ybm_helper_ui
